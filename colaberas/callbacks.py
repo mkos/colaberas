c70e0dd9cce5f65bc6c47a579df66665cc8ec6fe
Michal Kosinski <kosinski.michal@gmail.com>
2018-02-19 06:17:05 +0100

    add overriden Keras callbacks

---
+++ colaberas/callbacks.py
@@ -0,0 +1,23 @@
+from keras.callbacks import ModelCheckpoint, CSVLogger
+from colaberas.drive import upload_file
+
+
+class ModelCheckpointDriveUpload(ModelCheckpoint):
+    """ Overrides Keras' ModelCheckpoint class to save weights Google Drive as well. """
+
+    def __init__(self, filepath, drive_folder='', **kwargs):
+        super().__init__(filepath, **kwargs)
+        self.drive_folder = drive_folder
+        self.filepath = filepath
+
+    def on_epoch_end(self, epoch, logs=None):
+        super().on_epoch_end(epoch, logs)
+        upload_file(self.filepath)
+
+
+class CSVLoggerDriveUpload(CSVLogger):
+    """ Overrides Keras' CSVLogger class to save weights Google Drive as well. """
+
+    def on_epoch_end(self, epoch, logs=None):
+        super().on_epoch_end(epoch, logs)
+        upload_file(self.filename)

c27fd62bc9994a02f085d34ab489ba598d3745e3
Michal Kosinski <kosinski.michal@gmail.com>
2018-02-20 09:27:44 +0100

    add moar tests

--- tests/test_drive.py
+++ tests/test_drive.py
@@ -1,7 +1,8 @@
 from unittest import mock
 from unittest.mock import call
-from colaberas.drive import file_id_from_path
+from colaberas.drive import file_id_from_path, upload_file
 import pathlib
+import pytest
 
 
 def patch_find_id(filename, *args, **kwargs):
@@ -21,3 +22,15 @@ def test_file_id_from_path(find_id):
     find_id.assert_has_calls([call('test', 'root'), call('path', 123), call('file.txt', 345)])
     assert file_id == 567
     assert parent_id == 345
+
+
+@mock.patch.object('colaberas.drive.find_id', side_effect=patch_find_id)
+@mock.patch('googleapiclient.http.MediaFileUpload')
+@mock.patch('googleapiclient.discovery.build')
+#@mock.patch('googleapis.driveservice')
+@pytest.mark.parametrize('local_path,gdrive_path,result', [
+    ('local/file/path.txt', 'test/path/', None),
+#    ('local/file/path.txt', 'test/path/file.txt', None)
+])
+def test_upload_file(build, MediaFileUpload, find_id, local_path, gdrive_path, result):
+    upload_file(local_path, gdrive_path, mimetype='application/octet-stream')

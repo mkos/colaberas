a70d8ff8c81eb80fb45052ff75269ca2de803e53
Michal Kosinski <kosinski.michal@gmail.com>
2018-02-20 08:27:27 +0100

    add test_file_id_from_path

--- tests/test_drive.py
+++ tests/test_drive.py
@@ -1,1 +1,23 @@
-import pytest
+from unittest import mock
+from unittest.mock import call
+from colaberas.drive import file_id_from_path
+import pathlib
+
+
+def patch_find_id(filename, *args, **kwargs):
+    parent_id = args[0]
+    if filename == 'test' and parent_id == 'root':
+        return 123
+    if filename == 'path' and parent_id == 123:
+        return 345
+    if filename == 'file.txt' and parent_id == 345:
+        return 567
+
+
+@mock.patch('colaberas.drive.find_id', side_effect=patch_find_id)
+def test_file_id_from_path(find_id):
+    file_id, parent_id = file_id_from_path(pathlib.Path('test/path/file.txt'))
+
+    find_id.assert_has_calls([call('test', 'root'), call('path', 123), call('file.txt', 345)])
+    assert file_id == 567
+    assert parent_id == 345
